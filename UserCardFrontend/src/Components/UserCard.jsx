ae343720d4a6584a1b1d84f089fc2fcf354abac3
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-01-17 15:30:04 +0530

    enhance UserCard component and add Header with responsive menu--shadcn components also used

--- UserCardFrontend/src/Components/UserCard.jsx
+++ UserCardFrontend/src/Components/UserCard.jsx
@@ -1,20 +1,42 @@
+import { MapPinned } from "lucide-react";
+
 const UserCard = () => {
-  return (
-    <div className="h-90 w-65 bg-white shadow-lg p-4 m-3">
-      <div className="flex items-center justify-between h-12">
-        <h4 className="bg-green-500 text-white rounded-full font-bold text-xs p-1 m-1">
-          available
-        </h4>
-        <h4 className="text-sm text-black-500">$20/hr</h4>
-      </div>
-      <div className="middle-part flex items-center flex-col justify-center">
-        <img className="h-40 w-40 rounded-full object-contain" src="https://i.pinimg.com/1200x/17/c0/d1/17c0d1bfcef18ad4a83d5b5b95f328df.jpg" alt="" />
-        <h2 className="font-semibold text-lg font-sans">Aditya Binjagermath</h2>
-        <h4 className="font-light text-xs">UI/UX Designer</h4>
-        <p className="designation text-blue-300">Freelancer</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="h-105 w-80 bg-white shadow-lg p-4 m-3">
+            <div className="flex items-center justify-between h-12">
+                <h4 className="bg-green-500 text-white rounded-full font-bold text-xs px-2 py-1 m-1">
+                    available
+                </h4>
+                <h4 className="text-sm text-gray-500">$20/hr</h4>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+                <img
+                    className="h-40 w-40 rounded-full object-cover"
+                    src="https://i.pinimg.com/1200x/17/c0/d1/17c0d1bfcef18ad4a83d5b5b95f328df.jpg"
+                    alt="Aditya Binjagermath"
+                />
+                <h2 className="font-semibold text-lg font-sans">Aditya Binjagermath</h2>
+                <h4 className="font-light text-xs">UI/UX Designer</h4>
+                <p className="flex items-center gap-1 text-xs text-blue-600">
+                    <MapPinned size={14} className="text-green-500" />
+                    Freelancer
+                </p>
+            </div>
+            <div className="flex flex-wrap justify-center mt-4 text-sm font-semibold">
+                <button className="bg-white-200 border text-black-900 rounded-full px-4 py-1 m-1">UI</button>
+                <button className="bg-white-200 border text-black-900 rounded-full px-4 py-1 m-1">UX</button>
+                <button className="bg-white-200 border text-black-900 rounded-full px-4 py-1 m-1">frontend</button>
+                <button className="bg-blue-400 text-black-900 rounded-full px-4 py-1 m-1">+4</button>
+                <p className="mt-2 text-xs text-[10px] text-center text-gray-600">
+                    Aditya is a 22 years UI/UX designer, passionate about creating intuitive and engaging user experiences.
+                </p>
+            </div>
+            <div className="mt-4 flex flex-col items-center justify-center">
+               <hr className="w-30"/>
+               <button className="bg-white hover:bg-sky-500 hover:text-white hover:mt-0 w-80 h-9.5 text-black font-semibold text-[15px] px-4 py-1">VIEW PROFILE</button>
+            </div>
+        </div>
+    );
 };
 
 export default UserCard;
--- UserCardFrontend/src/Pages/UserPage.jsx
+++ UserCardFrontend/src/Pages/UserPage.jsx
@@ -3,6 +3,7 @@ import UserCard from '../Components/UserCard'
 const UserPage = () => {
   return (
     <div className='bg-black h-screen p-5 mb-5 items-center '>
+        <h1 className='text-center text-white font-bold text-2xl'>Profiles(27)</h1>
         <UserCard />
     </div>
   )
