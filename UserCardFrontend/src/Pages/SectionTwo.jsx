fbb4c53cd014d97f4facb6b693fa40f47ef37fde
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-01-19 16:26:35 +0530

    Add Navbar component to SectionTwo layout

--- UserCardFrontend/src/Pages/SectionTwo.jsx
+++ UserCardFrontend/src/Pages/SectionTwo.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../Components/SectionOne/Navbar'
 
 const SectionTwo = () => {
   return (
-    <div className='w-full h-screen bg-blue-400'></div>
+    <div className='w-full h-screen bg-blue-400'>
+      <Navbar/>
+    </div>
   )
 }
 
